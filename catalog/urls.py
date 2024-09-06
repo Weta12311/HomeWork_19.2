fcaed249b1faebee9d02b34a391762e3c81ed48b
Wetal <margenshteynv@gmail.com>
2024-09-06 21:40:06 +0300

    complite project

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
+from django.conf import settings
 
 
 app_name = CatalogConfig.name
@@ -8,4 +10,4 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("", home, name="index"),
     path("contacts/", contacts, name="contacts"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 
@@ -95,3 +96,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
