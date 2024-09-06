bc8aabf478daec86f43504463c3ce20df8ad4931
Wetal <margenshteynv@gmail.com>
2024-09-06 19:46:21 +0300

    add new models and settings for them

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание продукта"
+    )
+    image = models.ImageField(
+        upload_to="Product/image",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите изображение",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория продукта",
+        help_text="Введите категорию продукта",
+        blank=True,
+        null=True,
+        related_name='Продукты'
+    )
+    price = models.IntegerField(verbose_name="Цена", help_text="Введите цену продукта")
+    create_date = models.DateField(
+        blank=True, null=True, verbose_name="Дата создания записи"
+    )
+    changes_date = models.DateField(
+        blank=True, null=True, verbose_name="Дата изменения записи"
+    )
+    manufactured_at = models.DateField(blank=True, null=True, verbose_name="Дата производства продукта")
+
+    class Meta:
+        verbose_name = "Продукты"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "category", "create_date", "changes_date"]
+
+    def __str__(self):
+        return self.name
+
+
+
