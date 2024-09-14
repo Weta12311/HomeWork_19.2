fc013fba89d119caae2393b6d7ca1a54a01ac51e
Wetal <margenshteynv@gmail.com>
2024-09-14 11:45:03 +0300

    correct some fails

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -8,3 +8,10 @@ def media_filter(path):
     if path:
         return f'/media/{path}'
     return '#'
+
+
+@register.filter()
+def truncate_chars(text):
+    if len(text) > 100:
+        return text[:100]
+    return text
