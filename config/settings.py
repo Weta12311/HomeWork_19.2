ca381d1b5117e20b54a73496cd273c2c39d11842
Wetal <margenshteynv@gmail.com>
2024-10-24 19:13:11 +0300

    homework 23.2 questions 4

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,16 @@
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = "django-insecure-drikb)d(syzh5lqc67w9ob($e@g!r)r1m_!sej5h)%bu)i300$"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False) == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
@@ -62,11 +64,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "homework_20.1",
-        "USER": "postgres",
-        "PASSWORD": "5621",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
     }
 }
 
@@ -111,12 +113,12 @@ AUTH_USER_MODEL = "users.User"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "vitalymargenshtein@yandex.ru"
-EMAIL_HOST_PASSWORD = "mhjneeihywdigpkd"
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", False) == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", False) == "True"
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
@@ -125,6 +127,6 @@ CACHE_ENABLED = True
 if CACHE_ENABLED:
     CACHES = {
         "default": {"BACKEND": "django.core.cache.backends.redis.RedisCache",
-                    "LOCATION": "redis://localhost:6379"
+                    "LOCATION": os.getenv("LOCATION")
                     }
               }
